b4de77446ea74c2b93aa5a70bae5830bd6e7bcf5
Noskcaj <kcajdev@gmail.com>
2024-01-11 15:54:05 -0500

    Tweaks

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,14 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_ALPHAS_QWERTY
+#define MIRYOKU_TAP_QWERTY
+#define MIRYOKU_KLUDGE_MOUSEKEYSPR
+
+// clang-format off
+#define MIRYOKU_ALTERNATIVES_TAP_QWERTY \
+&kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
+&kp A,             &kp S,             &kp D,             &kp F,             &kp G,             &kp H,             &kp J,             &kp K,             &kp L,             &kp SQT,           \
+&kp Z,             &kp X,             &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         &kp DOT,           &kp SLASH,         \
+U_NP,              U_NP,              &kp ESC,           &kp SPACE,         &kp TAB,           &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
+// clang-format on
